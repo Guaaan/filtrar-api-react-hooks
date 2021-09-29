fe4e29dc41e9a6d42dd7661cd1d0479ca62c7fc2
guaaan <juampablo.txt@gmail.com>
2021-09-29 11:40:21 -0300

    filtrar por empresa y nombre

--- src/App.js
+++ src/App.js
@@ -6,59 +6,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const [usuarios, setUsuarios]= useState([]);
-  const [tablaUsuarios, setTablaUsuarios]= useState([]);
-  const [busqueda, setBusqueda]= useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  const [tablaUsuarios, setTablaUsuarios] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
-  const peticionGet=async()=>{
+  const peticionGet = async () => {
     await axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(response=>{
-      setUsuarios(response.data);
-      setTablaUsuarios(response.data);
-    }).catch(error=>{
-      console.log(error);
-    })
+      .then(response => {
+        setUsuarios(response.data);
+        setTablaUsuarios(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
   }
 
-  const handleChange=e=>{
+  const handleChange = e => {
     setBusqueda(e.target.value);
-    console.log("Búsqueda: "+e.target.value);
+    filtrar("Búsqueda: " + e.target.value);
   }
 
-  useEffect(()=>{
+  const filtrar = (terminados) => {
+    var resultadosBusqueda = tablaUsuarios.filter((elemento) => {
+      if (elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase()) || elemento.company.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ) {
+        return elemento;
+      }
+    });
+    setUsuarios(resultadosBusqueda);
+  }
+
+  useEffect(() => {
     peticionGet();
-  },[])
+  }, [])
   return (
     <div className="App">
       <div className="containerInput">
         <input
-        className="form-control inputBuscar"
-        velue={busqueda}
-        placeholder="Búsqueda por Nombre o Empresa"
-        onChange={handleChange}
+          className="form-control inputBuscar"
+          velue={busqueda}
+          placeholder="Búsqueda por Nombre o Empresa"
+          onChange={handleChange}
         />
         <button className="btn btn-success">
-          <FontAwesomeIcon icon={faSearch}/>
+          <FontAwesomeIcon icon={faSearch} />
         </button>
       </div>
 
       <div className="table-responsive">
         <table className="table table-sm table-bordered">
           <thead>
-           <tr>
-             <th>ID</th>
-             <th>Nombre</th>
-             <th>Teléfono</th>
-             <th>Nombre de Usuario</th>
-             <th>Correo</th>
-             <th>Sitio Web</th>
-             <th>Ciudad</th>
-             <th>Empresa</th>
-           </tr>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Teléfono</th>
+              <th>Nombre de Usuario</th>
+              <th>Correo</th>
+              <th>Sitio Web</th>
+              <th>Ciudad</th>
+              <th>Empresa</th>
+            </tr>
           </thead>
-            <tbody>
-              {usuarios && 
-              usuarios.map((usuario)=>(
+          <tbody>
+            {usuarios &&
+              usuarios.map((usuario) => (
                 <tr key={usuario.id}>
                   <td>{usuario.id}</td>
                   <td>{usuario.name}</td>
@@ -70,7 +80,7 @@ function App() {
                   <td>{usuario.company.name}</td>
                 </tr>
               ))}
-            </tbody>
+          </tbody>
         </table>
 
       </div>
