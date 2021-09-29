190b20bc9c3d586dc5a5c93956822ffe0761a25d
guaaan <juampablo.txt@gmail.com>
2021-09-29 11:24:05 -0300

    creada tabla de usuarios y agregados

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [usuarios, setUsuarios]= useState([]);
@@ -11,10 +13,16 @@ function App() {
   const peticionGet=async()=>{
     await axios.get("https://jsonplaceholder.typicode.com/users")
     .then(response=>{
-      console.log(response.data);
+      setUsuarios(response.data);
+      setTablaUsuarios(response.data);
     }).catch(error=>{
       console.log(error);
     })
+  }
+
+  const handleChange=e=>{
+    setBusqueda(e.target.value);
+    console.log("Búsqueda: "+e.target.value);
   }
 
   useEffect(()=>{
@@ -22,7 +30,50 @@ function App() {
   },[])
   return (
     <div className="App">
-      
+      <div className="containerInput">
+        <input
+        className="form-control inputBuscar"
+        velue={busqueda}
+        placeholder="Búsqueda por Nombre o Empresa"
+        onChange={handleChange}
+        />
+        <button className="btn btn-success">
+          <FontAwesomeIcon icon={faSearch}/>
+        </button>
+      </div>
+
+      <div className="table-responsive">
+        <table className="table table-sm table-bordered">
+          <thead>
+           <tr>
+             <th>ID</th>
+             <th>Nombre</th>
+             <th>Teléfono</th>
+             <th>Nombre de Usuario</th>
+             <th>Correo</th>
+             <th>Sitio Web</th>
+             <th>Ciudad</th>
+             <th>Empresa</th>
+           </tr>
+          </thead>
+            <tbody>
+              {usuarios && 
+              usuarios.map((usuario)=>(
+                <tr key={usuario.id}>
+                  <td>{usuario.id}</td>
+                  <td>{usuario.name}</td>
+                  <td>{usuario.phone}</td>
+                  <td>{usuario.username}</td>
+                  <td>{usuario.email}</td>
+                  <td>{usuario.website}</td>
+                  <td>{usuario.address.city}</td>
+                  <td>{usuario.company.name}</td>
+                </tr>
+              ))}
+            </tbody>
+        </table>
+
+      </div>
     </div>
   );
 }
